b45942fd49afae775aa80713eb2c981a6b666f66
Nelson Moncada <juniormoncada17@gmail.com>
2025-05-10 13:03:20 +0200

    signing commits

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -202,6 +202,7 @@ return {
 		callback = function()
 		    vim.keymap.set('n', '<leader>pc', '<cmd>PhpactorContextMenu<CR>', { buffer = true, desc = 'PHP Context Menu' })
 		    vim.keymap.set('n', '<leader>pi', '<cmd>PhpactorImportClass<CR>', { buffer = true, desc = 'Import Class' })
+
 		end
 	    })
 	end
